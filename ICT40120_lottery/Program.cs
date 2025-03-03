8fa16d2bc324c60fcbdb597eeb972947c6d05c45
joelt <joel.tan.92@gmail.com>
2025-03-04 01:29:09 +0800

    linear search through array

--- ICT40120_lottery/Program.cs
+++ ICT40120_lottery/Program.cs
@@ -8,18 +8,18 @@ int lowestValue = 0;
 int highestValue = 10;
 
 //This loop accepts the user's input 
-int number_of_value_iteration = 0;
-while (number_of_value_iteration < number_of_values)
-{
-    Console.WriteLine("number");
-    if (int.TryParse(Console.ReadLine(), out user_guess[number_of_value_iteration]))
-    {
-        number_of_value_iteration++;
-    }else
-    {
-        Console.WriteLine("Try again");
-    }
-}
+//int number_of_value_iteration = 0;
+//while (number_of_value_iteration < number_of_values)
+//{
+//    Console.WriteLine("number");
+//    if (int.TryParse(Console.ReadLine(), out user_guess[number_of_value_iteration]))
+//    {
+//        number_of_value_iteration++;
+//    }else
+//    {
+//        Console.WriteLine("Try again");
+//    }
+//}
 
 //this fills the target array with random numbers
 Random rnd = new Random();
@@ -38,23 +38,28 @@ void PrintArray(int[] array)
 }
 
 
-// Linear Search to see if it is within bounds. this searches for one
-int LinearSearch(int[] array, int valuetoSearch)
+// Linear Search searches throuhg the array
+int LinearSearch(int[] array, int[] arraytoSearch)
 {
+    int[] tempArray = new int[number_of_values];
+    int k = 0;
     for (int i = 0; i < array.Length; i++)
-        if (array[i] == valuetoSearch)
-        {
-            return i;
-        }
+        for (int j =0; j < arraytoSearch.Length; j++)
+            if (array[i] == arraytoSearch[j])
+            {
+                tempArray[k] = arraytoSearch[j];
+                k++;
+            }
     return -1;
 }
 
 Console.WriteLine("Found at");
-Console.WriteLine(LinearSearch(user_guess, 4));
+//Console.WriteLine(LinearSearch(user_guess, [1,2,3,4,5]));
+Console.WriteLine(LinearSearch([2,2,8,7,4], [1,2,3,4,5]));
 Console.WriteLine("----");
 
 Console.WriteLine("target array");
-//Array.Sort(target_array);
 PrintArray(target_array);
 Console.WriteLine("user guess");
-PrintArray(user_guess); 
+PrintArray(user_guess);
+
